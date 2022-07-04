bcb9e620ee5fa6f0ba1e5f599413404c07f27011
Kevin Daysal <kdaysal123@gmail.com>
2022-07-04 12:57:18 -0400

    Set background color to user-specified color from Start page (passed to 'Chat' component as props)
    Created new StyleSheet at bottom of the file; moved in-line style attributes over to the Stylesheet

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 // The application’s main Chat component that renders the chat UI
 export default class Chat extends React.Component {
   render() {
+    //Extract the user's name from the Start page
     let name = this.props.route.params.name;
 
+    //Set backgroundColor on Chat page to what the user selected on the Start page
+    const selectedBackgroundColor = this.props.route.params.selectedBackgroundColor;
+
+    //Sets the page title to the user's name
     this.props.navigation.setOptions({ title: name });
 
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.chatView} backgroundColor={selectedBackgroundColor}>
         <Text>Hi {name}, let's chat!</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  chatView: {
+    //backgroundColor: "red",
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
