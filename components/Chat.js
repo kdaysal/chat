7889bc9138d07dda842ff6d23b549176acb22704
Kevin Daysal <kdaysal123@gmail.com>
2022-07-04 14:04:19 -0400

    Resolved 'cannot update a component while rendering a different component' error
    -Resolved this by creating a new state for 'name' (the user's name) and setting/updating it within the componentDidMount() method instead of trying to do this in the render() method previously
    
    -Stackoverflow post suggested the error was throwing due to 'this.props.navigation.setOptions' not being wrapped in a useEffect hook (or in my case of a class component, the componentDidMount method)

--- components/Chat.js
+++ components/Chat.js
@@ -3,21 +3,35 @@ import { View, Text, StyleSheet } from 'react-native';
 
 // The application’s main Chat component that renders the chat UI
 export default class Chat extends React.Component {
-  render() {
+
+  constructor() {
+    super();
+    this.state = {
+      name: ""
+    }
+  };
+
+  componentDidMount() {
     //Extract the user's name from the Start page
     let name = this.props.route.params.name;
+    this.setState({ name: name })
+
+
+    //Sets the page title to the user's name
+    this.props.navigation.setOptions({ title: name });
+  }
+
+  render() {
 
     //Set backgroundColor on Chat page to what the user selected on the Start page
     const selectedBackgroundColor = this.props.route.params.selectedBackgroundColor;
 
-    //Sets the page title to the user's name
-    this.props.navigation.setOptions({ title: name });
 
     return (
       <View style={styles.chatView} backgroundColor={selectedBackgroundColor}>
-        <Text>Hi {name}, let's chat!</Text>
+        <Text>Hi {this.state.name}, let's chat!</Text>
       </View>
-    )
+    );
   }
 }
 
