fe4cd3c2b785e3fe99dfdb8c25e8f5cfe746ca24
Kevin Daysal <kdaysal123@gmail.com>
2022-07-16 15:21:31 -0400

    Completely restructured CustomActions component as a function component instead of class component
    -Previously as a class component, I was getting an error: 'this.actionSheet()...is not a function'.
    -Spent hours reworking the code, looking at Expo snacks (the CF snack example worked for me when I ran it in a separate folder, but I still got the error using the same syntax as in the example).
    -Tried deleting node_modules folder and package-lock.json and reinstalling npm, but couldn't resolve it yet
    -Downgrading to Expo v44.0.0 didn't work either

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,74 +1,85 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
-export default class CustomActions extends React.Component {
-  // constructor() {
-  //   super();
-  //   //initialize state
-  //   this.state = {
-  //   }
-  // }//end constructor
+export default function CustomActions(props) {
+  const { showActionSheetWithOptions } = useActionSheet();
 
-  onActionPress = () => {
-    const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
+  function onActionPress() {
+    const options = [
+      'Choose From Library',
+      'Take Picture',
+      'Send Location',
+      'Cancel'
+    ];
+
     const cancelButtonIndex = options.length - 1;
-    this.context.actionSheet().showActionSheetWithOptions(
-      {
-        options,
-        cancelButtonIndex,
-      },
+    showActionSheetWithOptions({
+      options,
+      cancelButtonIndex
+    },
       async (buttonIndex) => {
         switch (buttonIndex) {
           case 0:
-            console.log('user wants to pick an image');
+            console.log('user wants to pick an image1');
             return;
+          //        return imagePicker();
           case 1:
             console.log('user wants to take a photo');
             return;
+          //        return takePhoto();
           case 2:
-            console.log('user wants to get their location');
+            console.log('user wants to get theier location');
+            return;
+          //        return getLocation();
           default:
+            return;
         }
-      },
-    );
-  };
-
-  render() {
-    return (
-      <TouchableOpacity style={[styles.container]} onPress={this.onActionPress}>
-        <View style={[styles.wrapper, this.props.wrapperStyle]}>
-          <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
-        </View>
-      </TouchableOpacity>
-    );
+      }
+    )
   }
 
-}//end component
+  return (
+    <TouchableOpacity
+      style={[styles.container]}
+      onPress={onActionPress}
+    >
+      {/* Add accessibility props later ^*/}
+      <View
+        style={[styles.wrapper, props.wrapperStyle]}>
+        <Text style={[styles.iconText, props.iconTextStyle]}>+</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
 
-//creating the StyleSheet
 const styles = StyleSheet.create({
   container: {
     width: 26,
     height: 26,
     marginLeft: 10,
-    marginBottom: 10,
+    marginBottom: 10
   },
   wrapper: {
     borderRadius: 13,
     borderColor: '#b2b2b2',
     borderWidth: 2,
-    flex: 1,
+    flex: 1
   },
   iconText: {
     color: '#b2b2b2',
     fontWeight: 'bold',
     fontSize: 16,
     backgroundColor: 'transparent',
-    textAlign: 'center',
+    textAlign: 'center'
   },
-});
+})
 
 CustomActions.contextTypes = {
-  actionSheet: PropTypes.func,
-};
+  actionSheet: PropTypes.func
+}
