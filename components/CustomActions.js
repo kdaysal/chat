cbbec8f503e73e8b6671f74bcb462208c5e230b1
Kevin Daysal <kdaysal123@gmail.com>
2022-07-16 17:19:24 -0400

    Reverted back to a class component - resolved error by using 'connectActionSheet'
    -this resolved the previous error: 'this.actionSheet()...is not a function'

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,61 +1,96 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import * as Permissions from 'expo-permissions';
-import * as ImagePicker from 'expo-image-picker';
-import * as Location from 'expo-location';
-import MapView from 'react-native-maps';
-import { useActionSheet } from '@expo/react-native-action-sheet';
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
 
-export default function CustomActions(props) {
-  const { showActionSheetWithOptions } = useActionSheet();
+import firebase from 'firebase';
+import firestore from 'firebase';
 
-  function onActionPress() {
+//import permissions, image selector, and location features (gps)
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+
+//need this in order to build the ActionSheet
+import { connectActionSheet } from "@expo/react-native-action-sheet";
+
+// create a CustomAction component to show an ActionSheet with Options
+class CustomAction extends React.Component {
+  state = {
+    image: null,
+    location: null,
+  };
+
+  //TBD
+  imagePicker = () => {
+    console.log(`imagePicker running`)
+  };
+
+  //TBD
+  takePhoto = () => {
+    console.log(`takePhoto running`)
+  };
+
+  //TBD
+  getLocation = () => {
+    console.log(`getLocation running`)
+  };
+
+  // Create 4 options for the user to choose via showActionSheetWithOptions()
+  onActionPress = () => {
     const options = [
-      'Choose From Library',
-      'Take Picture',
-      'Send Location',
-      'Cancel'
+      "Choose Image From Library",
+      "Take Picture",
+      "Send Location",
+      "Cancel",
     ];
-
     const cancelButtonIndex = options.length - 1;
-    showActionSheetWithOptions({
-      options,
-      cancelButtonIndex
-    },
+    this.props.showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
       async (buttonIndex) => {
         switch (buttonIndex) {
           case 0:
-            console.log('user wants to pick an image1');
+            console.log(`user wants to select an image`)
+            return this.imagePicker();
             return;
-          //        return imagePicker();
           case 1:
-            console.log('user wants to take a photo');
+            console.log(`user wants to take a photo`)
+            //  return this.takePhoto();
             return;
-          //        return takePhoto();
           case 2:
-            console.log('user wants to get theier location');
+            console.log(`user wants to share location`)
             return;
-          //        return getLocation();
+          // return this.getLocation();
+
           default:
-            return;
+            console.log(`user wants to cancel!`);
         }
       }
-    )
-  }
+    );
+  };
 
-  return (
-    <TouchableOpacity
-      style={[styles.container]}
-      onPress={onActionPress}
-    >
-      {/* Add accessibility props later ^*/}
-      <View
-        style={[styles.wrapper, props.wrapperStyle]}>
-        <Text style={[styles.iconText, props.iconTextStyle]}>+</Text>
-      </View>
-    </TouchableOpacity>
-  )
+  render() {
+    return (
+      <TouchableOpacity
+        accessible={true}
+        accessibilityLabel="More options"
+        accessibilityHint="Here you can choose to send an image or your location."
+        style={styles.container}
+        onPress={this.onActionPress}
+      >
+        <View style={[styles.wrapper, this.props.wrapperStyle]}>
+          <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -63,23 +98,26 @@ const styles = StyleSheet.create({
     width: 26,
     height: 26,
     marginLeft: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   wrapper: {
     borderRadius: 13,
-    borderColor: '#b2b2b2',
+    borderColor: "#b2b2b2",
     borderWidth: 2,
-    flex: 1
+    flex: 1,
   },
   iconText: {
-    color: '#b2b2b2',
-    fontWeight: 'bold',
+    color: "#b2b2b2",
+    fontWeight: "bold",
     fontSize: 16,
-    backgroundColor: 'transparent',
-    textAlign: 'center'
+    backgroundColor: "transparent",
+    textAlign: "center",
   },
-})
+});
 
-CustomActions.contextTypes = {
-  actionSheet: PropTypes.func
-}
+CustomAction.contextTypes = {
+  actionSheet: PropTypes.func,
+};
+
+const CustomActions = connectActionSheet(CustomAction);
+export default CustomActions;
